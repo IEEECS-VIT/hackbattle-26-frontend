244efdfad083f5760704a47720e0a3c70192f29f
Gracy Mehndiratta <mehndirattagracy62@gmail.com>
2026-07-22 11:36:20 +0530

    chore:combing two components folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-"use client";
-
-import PokedexJudge from "@/components/judges";
 import AboutPage from "@/app/components/about";
+import PokedexJudge from "@/app/components/judges";
 
 export default function Home() {
   return (
-    <main className="w-full">
-    <AboutPage />
+    <main className="w-full h-screen overflow-y-scroll snap-y snap-mandatory">
+      <section className="w-full h-screen snap-start">
+        <AboutPage />
+      </section>
+      <section className="w-full h-screen snap-start">
+        <PokedexJudge />
+      </section>
     </main>
   );
 }
-
