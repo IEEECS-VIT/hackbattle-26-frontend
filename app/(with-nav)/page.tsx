9d61b25f23d3abb07afdf43c0443b376acda77cf
devatmanpal17 <devatmanpal17@gmail.com>
2026-09-09 20:49:59 +0530

    Restore tracks section and navigation link

--- app/(with-nav)/page.tsx
+++ app/(with-nav)/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         ]}
       />
       <AboutPage />
-      {/* <MarqueeDivider
+      <MarqueeDivider
         layers={[
           {
             src: "/vine-marquee-thick.png",
@@ -39,7 +39,6 @@ export default function Home() {
       />
 
       <MapScene />
-     */ }
       <MarqueeDivider
         layers={[
           {
