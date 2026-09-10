db6b8cf21614fd3c72b8935bc0dd14ffde07eece
devatmanpal17 <devatmanpal17@gmail.com>
2026-09-10 20:04:48 +0530

    Replace desktop hero background video

--- components/home.tsx
+++ components/home.tsx
@@ -107,7 +107,7 @@ const Hero = () => {
           aria-hidden="true"
           className="pointer-events-none absolute top-0 left-0 z-0 h-full w-full select-none object-cover"
         >
-          <source src="/edit.webm" type="video/webm" />
+          <source src="/hackbattle-new-video.mp4" type="video/mp4" />
           <div className="w-full h-full bg-cyan-600"></div>
         </video>
         <div className="absolute inset-0 bg-blue-900/10 z-10"></div>
